77dbf2a5b6699ab10b9d7a90bf5000a9b7c57986
Adri <jevurah@gmail.com>
2023-05-14 03:05:06 +0200

    Modificar mensaje en rama funcion

--- PrimerGit/src/primergit/PrimerGit.java
+++ PrimerGit/src/primergit/PrimerGit.java
@@ -3,8 +3,7 @@ package primergit;
 public class PrimerGit {
 
     public static void main(String[] args) {
-        System.out.println("He cambiado el mensaje para probar esta nueva "
-                + "versión del proyecto en desarrollo");
+        System.out.println("Hola! soy una función!");
     }
 
 }
