58d6f63ad9341f5b8387d50bf163ce2b2ffa6ca8
Adri <jevurah@gmail.com>
2023-05-14 02:42:24 +0200

    Modificar mensaje en la versión de desarrollo

--- PrimerGit/src/primergit/PrimerGit.java
+++ PrimerGit/src/primergit/PrimerGit.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package primergit;
 
-/**
- *
- * @author Uvi
- */
 public class PrimerGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("He cambiado el mensaje para probar esta nueva "
+                + "versión del proyecto en desarrollo");
     }
-    
+
 }
