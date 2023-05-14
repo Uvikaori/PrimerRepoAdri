e98878043aefab455fa889c196867e39e08c97c1
Adri <jevurah@gmail.com>
2023-05-14 02:57:34 +0200

    Modificar mensaje en la versión de producción

--- PrimerGit/src/primergit/PrimerGit.java
+++ PrimerGit/src/primergit/PrimerGit.java
@@ -3,8 +3,8 @@ package primergit;
 public class PrimerGit {
 
     public static void main(String[] args) {
-        System.out.println("He cambiado el mensaje para probar esta nueva "
-                + "versión del proyecto en desarrollo");
+        System.out.println("Esta es una versión optimizada para "
+                + "producción del proyecto");
     }
 
 }
